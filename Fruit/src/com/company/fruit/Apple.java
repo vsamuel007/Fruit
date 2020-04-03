89d3f0760a6ac21a993c47357d6fca10a97b3f36
vsamuel <varghese.samuel@capgemini.com>
2020-04-03 22:35:01 +0530

    Apple feature commit from John

--- Fruit/src/com/company/fruit/Apple.java
+++ Fruit/src/com/company/fruit/Apple.java
@@ -3,6 +3,11 @@ package com.company.fruit;
 public class Apple implements IFruit {
     @Override
     public String getName() {
-        return "Apple";
+        return "Apple Veg";
+    }
+
+    public void garbage {
+        int x = 1;
+        int y = 1;
     }
 }
